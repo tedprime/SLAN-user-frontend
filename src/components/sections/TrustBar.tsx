66f8fcd69b170b00988aaac69a66c5569a7891fc
E-R-I-X <olugbemide.eri@gmail.com>
2026-05-26 15:37:37 +0100

    addedlanding page

--- src/components/sections/TrustBar.tsx
+++ src/components/sections/TrustBar.tsx
@@ -24,27 +24,28 @@ export default function TrustBar() {
           Endorsed &amp; Trusted By
         </p>
 
-        <div className="flex overflow-x-auto sm:overflow-x-visible sm:grid sm:grid-cols-4 items-center gap-8 sm:gap-12 lg:gap-16 max-w-5xl mx-auto pb-4 sm:pb-0 scrollbar-hide">
+        <div className="flex overflow-x-auto sm:overflow-x-visible sm:grid sm:grid-cols-4 items-start gap-8 sm:gap-12 lg:gap-16 max-w-5xl mx-auto pb-4 sm:pb-0 scrollbar-hide">
           {endorsers.map((org) => (
             <div
               key={org.name}
               className="flex flex-col items-center gap-3 sm:gap-4 shrink-0 sm:shrink"
             >
-              {org.isWordmark ? (
-                <div className="w-20 h-20 sm:w-24 sm:h-24 lg:w-28 lg:h-28 flex items-center justify-center">
+              {/* Fixed-height container so all labels align at the same vertical position */}
+              <div className="w-24 h-24 sm:w-28 sm:h-28 flex items-center justify-center">
+                {org.isWordmark ? (
                   <img
                     src={org.logo}
                     alt={org.name}
                     className="w-full h-auto object-contain"
                   />
-                </div>
-              ) : (
-                <img
-                  src={org.logo}
-                  alt={org.name}
-                  className="w-20 h-20 sm:w-24 sm:h-24 lg:w-28 lg:h-28 rounded-full object-cover"
-                />
-              )}
+                ) : (
+                  <img
+                    src={org.logo}
+                    alt={org.name}
+                    className="w-full h-full rounded-full object-cover"
+                  />
+                )}
+              </div>
               <span className="font-label font-semibold text-sm lg:text-base text-tertiary-500 whitespace-nowrap">
                 {org.name}
               </span>
